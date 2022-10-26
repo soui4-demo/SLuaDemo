67b682cc7ba31bbbc5a35fd18e4a187901c7b821
huangjianxiong1 <huangjianxiong1@yy.com>
2022-10-26 18:39:44 +0800

    impl listview demo

--- Debug/main.lua
+++ Debug/main.lua
@@ -4,6 +4,46 @@ function onBtnClose(e)
 	hostWnd:DestroyWindow();
 end
 
+function onLvBtnClick(e)
+	slog("onLvBtnClick");
+
+end
+
+function lv_getView(strCtx, iPos, pItem, xmlTemplate)
+	slog("lv_getView ipos:" .. iPos);
+	if(pItem:GetChildrenCount() == 0) then
+		pItem:InitFromXml(xmlTemplate);
+	end
+	local pTxt = pItem:FindChildByNameA("lv_txt1",-1);
+	pTxt:SetWindowText(T("hello lua " .. iPos));
+	local pBtn = pItem:FindChildByNameA("lv_btn1",-1);
+	SubscribeWindowEvent(pBtn,10000,"onLvBtnClick");
+end
+
+function lv_getCount(strCtx)
+	return 100;
+end
+
+function onBtnInitLv(e)
+	slog("init listview");
+	local hostWnd = GetHostWndFromObject(e:Sender());
+	toSWindow(e:Sender());
+	local lvTst = hostWnd:FindIChildByName(L"lv_test",-1);
+	slog("init listview, FindIChildByName done");
+	local ilvTst = QiIListView(lvTst);
+	slog("init listview, QiIListView done");
+
+	local adapter = CreateLvAdapter("test_lv");
+	adapter:initCallback(0,"lv_getView");
+	adapter:initCallback(1,"lv_getCount");
+
+	slog("init listview, init callbacks done");
+	ilvTst:SetAdapter(adapter);
+	slog("init listview, SetAdapter done");
+	adapter:Release();
+	ilvTst:Release();
+end
+
 function main(hinst,strWorkDir,strArgs)
 	slog("main start");
 	local souiFac = CreateSouiFactory();
@@ -19,10 +59,13 @@ function main(hinst,strWorkDir,strArgs)
 	hostWnd:Create(hwnd,0,0,0,0);
 	hostWnd:ShowWindow(1); --1==SW_SHOWNORMAL
 	local btnClose = hostWnd:FindIChildByName(L"btn_close",-1);
-	local slot = CreateEventSlot("onBtnClose");
-	btnClose:SubscribeEvent(10000,slot);--10000 == EVT_CMD
-	slot:Release();
+	SubscribeWindowEvent(btnClose,10000,"onBtnClose");
+
+	local btnInitLv = hostWnd:FindIChildByName(L"btn_init_lv",-1);
+	SubscribeWindowEvent(btnInitLv,10000,"onBtnInitLv");
+	
 	souiFac:Release();
+	
 	slog("main done");
 	return theApp:Run(hostWnd:GetHwnd());
 end
